adcf7fc35a18d2bd8401b5d6e7853bc99d3fbc71
Alexander Kolotov <alexandr.kolotov@gmail.com>
2023-04-20 14:54:14 +0300

    connector config is made through constructor params

--- feeding/connector.py
+++ feeding/connector.py
@@ -1,7 +1,5 @@
 from requests.auth import AuthBase
 
-from utils.settings.feeding import FeedingServiceSettings
-
 from pydantic import BaseModel
 
 class SimpleBearerAuth(AuthBase):
@@ -12,20 +10,14 @@ class SimpleBearerAuth(AuthBase):
         r.headers['Authorization'] = f'Bearer {self.token}'
         return r
 
-class ConnectorConfig(BaseModel):
-    base_url: str
-    upload_path: str
-    upload_token: str
-    health_path: str
-
 class BaseConnector:
     _service_url: str
     _upload_path: str
     _health_path: str
     _bearer_auth: SimpleBearerAuth
 
-    def __init__(self, config: ConnectorConfig):
-        self._service_url = config.base_url
-        self._upload_path = config.upload_path
-        self._health_path = config.upload_token
-        self._bearer_auth = SimpleBearerAuth(config.health_path)
+    def __init__(self, base_url: str, upload_path: str, upload_token: str, health_path: str):
+        self._service_url = base_url
+        self._upload_path = upload_path
+        self._health_path = upload_token
+        self._bearer_auth = SimpleBearerAuth(health_path)
--- main-harvester.py
+++ main-harvester.py
@@ -5,8 +5,6 @@ from bobstats.stats import Stats
 from bobstats.db import DBAdapter
 from bobstats.feeding import prepare_data_for_feeding, BobStatsConnector
 
-from feeding.connector import ConnectorConfig
-
 from utils.logging import error, info
 from utils.misc import every
 
@@ -25,12 +23,10 @@ class BobStats:
         self._stats = Stats(settings)
         self._db = DBAdapter(settings)
         self._connector = BobStatsConnector(
-            ConnectorConfig(
-                base_url=settings.feeding_service_url,
-                upload_path=settings.feeding_service_path,
-                upload_token=settings.feeding_service_upload_token,
-                health_path=settings.feeding_service_health_path
-            )
+            base_url=settings.feeding_service_url,
+            upload_path=settings.feeding_service_path,
+            upload_token=settings.feeding_service_upload_token,
+            health_path=settings.feeding_service_health_path
         )
         self._measurements_interval = settings.measurements_interval
         self._monitor_interval = settings.feeding_service_monitor_interval
